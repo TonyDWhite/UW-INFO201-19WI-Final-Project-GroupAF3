c8e387ab21894e96ad71e7134fadb15d95358bf5
Aaron Zhao <zhiyuz2@uw.edu>
2019-03-09 10:31:59 -0800

    Added server and ui files

---
+++ my_server.R
@@ -0,0 +1,10 @@
+#Final Project my_server file
+library("shiny")
+library("ggplot2")
+library("dplyr")
+
+
+my_server <- function(input, output){
+  
+  
+}
---
+++ my_ui.R
@@ -0,0 +1,11 @@
+#Final Project ui file
+
+library("shiny")
+library("ggplot2")
+library("dplyr")
+
+
+my_ui <- fluidPage(
+  
+  
+)
